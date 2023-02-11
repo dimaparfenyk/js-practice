51491ec1a73e62a497d3f3b9d49b02a57ec71c09
Dmitrij <dimaparfenyk1990@gmail.com>
2023-02-11 20:21:14 +0200

    update color-switcher

--- src/js/async/01-color-switcher.js
+++ src/js/async/01-color-switcher.js
@@ -5,12 +5,12 @@ const refs={
 };
 
 let intervalId = null;
-
 refs.closeBtn.disabled = true;
+refs.closeBtn.classList.add("is-active");
 
 refs.startBtn.addEventListener('click', () => {
     
-    toggleBtn(refs.closeBtn,refs.startBtn)
+    toggleBtn(refs.closeBtn, refs.startBtn);
 
     intervalId = setInterval(() => {
         refs.body.style.backgroundColor = getRandomHexColor();
@@ -19,14 +19,45 @@ refs.startBtn.addEventListener('click', () => {
 
 refs.closeBtn.addEventListener('click', () => {
    clearInterval(intervalId);
-   toggleBtn(refs.startBtn, refs.closeBtn)
+    toggleBtn(refs.startBtn, refs.closeBtn);
 });
 
 function toggleBtn(activeBtn, disactiveBtn) {
     activeBtn.disabled = false;
     disactiveBtn.disabled = true;
+    activeBtn.classList.toggle("is-active");
+    disactiveBtn.classList.toggle("is-active");
 };
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+// const colorGenerator = {
+
+//     intervalId: null,
+//     isActive: false,
+   
+//     start() {
+//         if (this.isActive) return;
+        
+//         refs.startBtn.disabled = true;
+//         refs.stopBtn.disabled = false;
+//         this.isActive = true;
+
+//         this.intervalId = setInterval(() => {
+//             refs.body.style.background = getRandomHexColor();
+//         }, 1000);
+//     },
+
+//     stop() {
+//         clearInterval(this.intervalId);
+//         this.isActive = false;
+//         refs.stopBtn.disabled = true;
+//         refs.startBtn.disabled = false;
+//     },
+// };
+
+// refs.startBtn.addEventListener('click', () => { colorGenerator.start() });
+// refs.stopBtn.addEventListener('click', () => { colorGenerator.stop() });
